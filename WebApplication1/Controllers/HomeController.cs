0c2aa114fee38bb49f5588d88a0f1ad57e4453a2
bohatermateusz <bohatermateusz@gmail.com>
2020-12-16 17:01:26 +0100

    Added default cookies for language culture change and new View

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 
@@ -25,6 +27,31 @@ namespace WebApplication1.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        protected void CheckBox_Click(object sender, EventArgs e)
+        {
+
+
+            var dil = "en-US";
+            //var ci = new CultureInfo(dil); //TO_DO Route culture 
+            //Thread.CurrentThread.CurrentUICulture = ci;
+            //Thread.CurrentThread.CurrentCulture = ci;
+            //Session["culture"] = ci;
+
+            //Sets the cookie that is to be used by Global.asax
+            HttpCookie cookie = new HttpCookie("CultureInfo");
+            cookie.Value = dil;
+            Response.Cookies.Add(cookie);
+
+            //Set the culture and reload the page for immediate effect. 
+            //Future effects are handled by Global.asax
+            Thread.CurrentThread.CurrentCulture =
+                new CultureInfo(dil);
+            Thread.CurrentThread.CurrentUICulture =
+                new CultureInfo(dil);
+            Server.Transfer(Request.Path);
+
         }
     }
 }
